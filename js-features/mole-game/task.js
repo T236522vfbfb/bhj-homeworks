ff2d00d8ce593a527f6871ac5ef9dad99f92f078
Name Surname <email@email.com>
2023-12-07 20:31:07 +0300

    07.12.2023

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,20 +1,34 @@
 const holes = document.querySelectorAll('.hole')
+const deadText = document.getElementById('dead')
+const lostText = document.querySelector('#lost')
+
 let kills = 0
 let missed = 0
+
+// const restartGame = alert_text => {
+function restartGame(alert_text) {
+    alert(alert_text)
+    kills = 0
+    deadText.innerHTML = kills
+    missed = 0
+    lostText.innerHTML = missed
+}
 
 holes.forEach( (hole) => {
     hole.addEventListener('click', () => {
         if(hole.classList.contains('hole_has-mole')) {
             kills++
+            deadText.innerHTML = kills
             console.log(`kills: ${kills}`)
             if (kills == 10) {
-                alert("Победа!")
+                restartGame("Победа!")
             }
         } else {
             missed++
+            lostText.innerHTML = missed
             console.log(`missed: ${missed}`)
             if (missed == 5) {
-                alert("Поражение")
+                restartGame("Поражение")
             }
         }
     })
