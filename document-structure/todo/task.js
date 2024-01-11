9aed58b64e68a874c88aeab1b796cb4af2c41bec
Name Surname <email@email.com>
2024-01-11 20:58:59 +0300

    11.01.204 pfl 22

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,27 +1,30 @@
-let inputBox = document.getElementById("task__input");
-let tasksList = document.getElementById("tasks__list");
+const inputBox = document.getElementById("task__input");
+const tasksList = document.getElementById("tasks__list");
 
 const removeTask = e => {
 	e.target.closest(".task").remove();
 }
 
-const addTask = e => {
-	tasksList.innerHTML +=
-		`<div class="task">
-			<div class="task__title">
-				${inputBox.value}
-			</div>
-			<a href="#" class="task__remove">&times;</a>
-		</div>`;
-
+const addTask = () => {
+	const taskText = inputBox.value.trim()
 	inputBox.value = "";
+	
+	if(taskText == '') return
 
-  	[...(tasksList.getElementsByClassName("task__remove"))].forEach(element => {
-		element.addEventListener("click", removeTask)
-	});
+	tasksList.innerHTML +=
+	`<div class="task">
+		<div class="task__title">
+			${taskText}
+		</div>
+		<a href="#" class="task__remove">&times;</a>
+	</div>`;
 
 	e.preventDefault();
 }
+
+tasksList.addEventListener("click", (e) => {
+	if (e.target.classList.contains('task__remove')) e.target.closest(".task").remove();
+})
 
 document.getElementById("tasks__add").addEventListener("click", addTask);
 // inputBox.addEventListener("keypress", e => {
