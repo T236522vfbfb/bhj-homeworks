c0cd937025a8b8d9672cf2cea0175129ee5abbcf
Name Surname <email@email.com>
2024-01-11 20:29:37 +0300

    11.01.2024

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -4,12 +4,15 @@ const toolTipList = Array.from(document.querySelectorAll('a.has-tooltip'));
 let innerTooltipText;
 
 toolTipList.forEach(function (tip) {
-    tip.onclick = function (event) {
+    tip.addEventListener('click', (event) => {
+        event.preventDefault()
+
         let tooltip = document.querySelector('div.tooltip_active');
         let topCoordinate = tip.getBoundingClientRect().top < window.innerHeight / 2 ?  tip.getBoundingClientRect().bottom + 10 : tip.getBoundingClientRect().top - 30;
         let leftCoordinate = tip.getBoundingClientRect().left < window.innerWidth / 2 ? tip.getBoundingClientRect().left + 10 : tip.getBoundingClientRect().left - 10;
         let html = `<div class="tooltip tooltip_active" style="left: ${leftCoordinate}px; top: ${topCoordinate}px">${tip.title}</div>`;
-        if (tooltip && event.path[0].innerText == innerTooltipText) {
+        
+        if (tooltip && event.target.innerText == innerTooltipText) {
             tooltip.classList.toggle('tooltip_active');
         } else if (tooltip) {
             tooltip.style.left = `${leftCoordinate}px`;
@@ -18,7 +21,28 @@ toolTipList.forEach(function (tip) {
         } else {
             tip.insertAdjacentHTML('afterend', html);
         };
-        innerTooltipText = event.path[0].innerText;
-        return false;
-    };
+
+        innerTooltipText = event.target.innerText;
+    })
+    
+    // tip.onclick = function (event) {
+    //     event.preventDefault()
+    //     let tooltip = document.querySelector('div.tooltip_active');
+    //     let topCoordinate = tip.getBoundingClientRect().top < window.innerHeight / 2 ?  tip.getBoundingClientRect().bottom + 10 : tip.getBoundingClientRect().top - 30;
+    //     let leftCoordinate = tip.getBoundingClientRect().left < window.innerWidth / 2 ? tip.getBoundingClientRect().left + 10 : tip.getBoundingClientRect().left - 10;
+    //     let html = `<div class="tooltip tooltip_active" style="left: ${leftCoordinate}px; top: ${topCoordinate}px">${tip.title}</div>`;
+        
+    //     if (tooltip && event.path[0].innerText == innerTooltipText) {
+    //         tooltip.classList.toggle('tooltip_active');
+    //     } else if (tooltip) {
+    //         tooltip.style.left = `${leftCoordinate}px`;
+    //         tooltip.style.top = `${topCoordinate}px`;
+    //         tooltip.textContent = `${tip.title}`;
+    //     } else {
+    //         tip.insertAdjacentHTML('afterend', html);
+    //     };
+
+    //     innerTooltipText = event.path[0].innerText;
+    //     // return false;
+    // };
 });
